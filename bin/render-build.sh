7dfdb4a9aca28d047bf196e1f41bec63ba0434ae
Will <willchesson@gmail.com>
2023-02-07 14:49:59 -0500

    db reset render

--- bin/render-build.sh
+++ bin/render-build.sh
@@ -11,5 +11,5 @@ cp -a client/build/. public/
 bundle install
 # bundle exec rake assets:precompile
 # bundle exec rake assets:clean
-bundle exec rake db:migrate
+bundle exec rake db:reset db:migrate db:seed DISABLE_DATABASE_ENVIRONMENT_CHECK=1
 # bundle exec rake db:seed # if you have seed data, run this command for the initial deploy only
